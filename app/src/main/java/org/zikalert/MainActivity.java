9592b11e18e1c89cc08d70e8830960186f3afe4a
luizcieslak <cieslakluiz@gmail.com>
2016-03-18 19:07:03 -0400

    first commit

---
+++ app/src/main/java/org/zikalert/MainActivity.java
@@ -0,0 +1,200 @@
+package org.zikalert;
+
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        //Set the TabLayout and link it with the ViewPager.
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+            //Open the Settings page
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Fragment for Informations section.
+     */
+    public static class InformationsFragment extends Fragment {
+
+        public InformationsFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment.
+         */
+        public static InformationsFragment newInstance() {
+            InformationsFragment fragment = new InformationsFragment();
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_informations, container, false);
+            return rootView;
+        }
+    }
+
+    /**
+     * Fragment for News section.
+     */
+    public static class NewsFragment extends Fragment {
+
+        public NewsFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment.
+         */
+        public static NewsFragment newInstance() {
+            NewsFragment fragment = new NewsFragment();
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_news, container, false);
+            return rootView;
+        }
+    }
+
+
+    /**
+     * Fragment for Checklist section.
+     */
+    public static class ChecklistFragment extends Fragment {
+
+        public ChecklistFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment.
+         */
+        public static ChecklistFragment newInstance() {
+            ChecklistFragment fragment = new ChecklistFragment();
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_checklist, container, false);
+            return rootView;
+        }
+    }
+
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            switch (position){
+                case 0:
+                    return InformationsFragment.newInstance();
+                case 1:
+                    return NewsFragment.newInstance();
+                case 2:
+                    return ChecklistFragment.newInstance();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "Informations";
+                case 1:
+                    return "News";
+                case 2:
+                    return "Checklist";
+            }
+            return null;
+        }
+    }
+}
