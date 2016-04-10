354a7b2dcf685c0340f6b77a8ac81e785569d75e
luizcieslak <cieslakluiz@gmail.com>
2016-04-10 15:32:44 -0400

    added Webhose.io library, Google API client library and tested the retrive of information by using AsyncTask

--- app/src/main/java/org/zikalert/fragments/NewsFragment.java
+++ app/src/main/java/org/zikalert/fragments/NewsFragment.java
@@ -4,13 +4,22 @@ package org.zikalert.fragments;
  * Created by Luiz Fernando on 3/18/2016.
  */
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.buzzilla.webhose.client.WebhoseClient;
+import com.buzzilla.webhose.client.WebhosePost;
+import com.buzzilla.webhose.client.WebhoseResponse;
 
 import org.zikalert.R;
+
+import java.io.IOException;
 
 /**
  * Fragment for News section.
@@ -25,5 +34,42 @@ public class NewsFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_news, container, false);
         return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        new GetNews().execute();
+
+    }
+
+    class GetNews extends AsyncTask<Void,Void,WebhoseResponse>{
+        @Override
+        protected WebhoseResponse doInBackground(Void... params) {
+            WebhoseResponse r = null;
+
+            try {
+                WebhoseClient client = new WebhoseClient("fefad8c8-e6e8-43f8-986e-c8b26946feee");
+                r = client.search("zika");
+
+
+
+            } catch (IOException e) {
+                Log.d("NewsFragment", "onActivityCreated: " + e.getMessage());
+            }
+
+            return r;
+        }
+
+        @Override
+        protected void onPostExecute(WebhoseResponse webhoseResponse) {
+            if(webhoseResponse != null){
+                WebhosePost p = webhoseResponse.posts.get(0);
+                TextView title = (TextView) getView().findViewById(R.id.news_title);
+                title.setText(p.title);
+            }
+
+        }
     }
 }
