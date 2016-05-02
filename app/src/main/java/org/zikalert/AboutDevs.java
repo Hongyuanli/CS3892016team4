8a44848a6b7e4137b5bd4ca6d305cae74a34f763
Luiz Cieslak <cieslakluiz@gmail.com>
2016-05-02 02:05:34 -0400

    Enhanced about page

--- app/src/main/java/org/zikalert/AboutDevs.java
+++ app/src/main/java/org/zikalert/AboutDevs.java
@@ -1,9 +1,14 @@
 package org.zikalert;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import org.zikalert.R;
 
@@ -22,7 +27,30 @@ public class AboutDevs extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         //The Default title is the name of the class, so we need to change it:
-        getSupportActionBar().setTitle("About the Developers");
+        getSupportActionBar().setTitle("About");
+
+        ImageView iconGithub = (ImageView) findViewById(R.id.iconGithub);
+        iconGithub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://github.com/paceuniversity/CS3892016team4";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
+        TextView references = (TextView) findViewById(R.id.textViewReferences);
+        references.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://pastebin.com/K6NjBAbV";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
     }
 
 }
